63502edeac33cffd3d7e971e3a3859fe3a43373c
Gustavo Henke <guhenke@gmail.com>
2019-09-18 08:48:32 +1000

    test(auth): fix all broken tests :)

--- auth/auth.test.ts
+++ auth/auth.test.ts
@@ -38,10 +38,10 @@ describe("#onAuthStateChange()", () => {
     const auth = new MockAuth(app);
     const listener = jest.fn();
 
-    const user = await auth.signInAnonymously();
+    const credential = await auth.signInAnonymously();
     auth.onAuthStateChanged(listener);
 
-    expect(listener).toHaveBeenCalledWith(user);
+    expect(listener).toHaveBeenCalledWith(credential.user);
   });
 
   it("adds a listener to auth state changes", async () => {
@@ -49,9 +49,9 @@ describe("#onAuthStateChange()", () => {
     const listener = jest.fn();
     auth.onAuthStateChanged(listener);
 
-    const user = await auth.signInAnonymously();
+    const credential = await auth.signInAnonymously();
     expect(listener).toHaveBeenCalledTimes(2);
-    expect(listener).toHaveBeenCalledWith(user);
+    expect(listener).toHaveBeenCalledWith(credential.user);
   });
 
   it("doesn't invokes the listener after it's disposed", async () => {
@@ -70,19 +70,20 @@ describe("#onAuthStateChange()", () => {
 describe("#signInAnonymously()", () => {
   it("signs in and returns user", async () => {
     const auth = new MockAuth(app);
-    const user = await auth.signInAnonymously();
+    const credential = await auth.signInAnonymously();
 
-    expect(user.isAnonymous).toBe(true);
-    expect(auth.currentUser).toBe(user);
+    expect(credential.user).not.toBeNull();
+    expect(credential.user!.isAnonymous).toBe(true);
+    expect(auth.currentUser).toBe(credential.user);
   });
 
   it("keeps same user signed in if it's anonymous", async () => {
     const auth = new MockAuth(app);
-    const user1 = await auth.signInAnonymously();
-    const user2 = await auth.signInAnonymously();
+    const credential1 = await auth.signInAnonymously();
+    const credential2 = await auth.signInAnonymously();
 
-    expect(user1).toBe(user2);
-    expect(auth.currentUser).toBe(user1);
+    expect(credential1).toEqual(credential2);
+    expect(auth.currentUser).toBe(credential1.user);
   });
 });
 
@@ -103,11 +104,11 @@ describe("#signInWithEmailAndPassword", () => {
 
   it("signs in with the given e-mail and password", async () => {
     const auth = new MockAuth(app);
-    const user1 = auth.store.add({ email: "foo@bar.com", password: "baz" });
+    const user = auth.store.add({ email: "foo@bar.com", password: "baz" });
 
-    const user2 = await auth.signInWithEmailAndPassword("foo@bar.com", "baz");
-    expect(user2).toEqual(user1);
-    expect(auth.currentUser).toEqual(user2);
+    const credential = await auth.signInWithEmailAndPassword("foo@bar.com", "baz");
+    expect(credential.user).toEqual(user);
+    expect(auth.currentUser).toEqual(credential.user);
   });
 });
 
@@ -118,7 +119,7 @@ describe("#signInWithPopup()", () => {
     const auth = new MockAuth(app);
     auth.mockSocialSignIn(provider).respondWithUser("Foo", "foo@bar.com");
 
-    const user = await auth.signInWithPopup(provider);
+    const { user } = await auth.signInWithPopup(provider);
     expect(auth.currentUser).toBe(user);
     expect(user).toHaveProperty("displayName", "Foo");
     expect(user).toHaveProperty("email", "foo@bar.com");
@@ -142,10 +143,10 @@ describe("#signInWithPopup()", () => {
     auth.mockSocialSignIn(provider).respondWithUser("Foo", "foo@bar.com");
     auth.mockSocialSignIn(provider).respondWithUser("Foo", "foo@bar.com");
 
-    const user1 = await auth.signInWithPopup(provider);
-    const user2 = await auth.signInWithPopup(provider);
-    expect(user1).toEqual(user2);
-    expect(auth.currentUser).toBe(user2);
+    const credential1 = await auth.signInWithPopup(provider);
+    const credential2 = await auth.signInWithPopup(provider);
+    expect(credential1).toEqual(credential2);
+    expect(auth.currentUser).toBe(credential2.user);
   });
 
   it("throws if email already exists for another account", () => {
