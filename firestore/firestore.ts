31ed549b0d6df56a488cb16a4823fe4f3f0477dd
Gustavo Henke <guhenke@gmail.com>
2020-05-23 02:19:13 +1000

    style(firestore): reorder methods for less diff LOC

--- firestore/firestore.ts
+++ firestore/firestore.ts
@@ -1,25 +1,43 @@
 import * as firebase from "firebase";
-import { MockDocumentReference } from "./document-reference";
-import { DEFAULT_DATA_CONVERTER } from "./data-converter";
+import { MockApp } from "../app";
 import { MockCollectionReference } from "./collection-reference";
+import { DEFAULT_DATA_CONVERTER } from "./data-converter";
+import { MockDocumentReference } from "./document-reference";
 
 export class MockFirestore implements firebase.firestore.Firestore {
+  // ?
+  INTERNAL: { delete: () => Promise<void> };
+
   private id = 0;
   public readonly documentData = new Map<string, firebase.firestore.DocumentData>();
   public readonly collectionDocuments = new Map<string, string>();
 
-  constructor(public readonly app: firebase.app.App) {}
+  constructor(public readonly app: MockApp) {}
 
   nextId() {
     return "__id" + this.id++;
   }
 
-  settings(settings: firebase.firestore.Settings): void {
+  batch(): firebase.firestore.WriteBatch {
     throw new Error("Method not implemented.");
   }
-  enablePersistence(settings?: firebase.firestore.PersistenceSettings | undefined): Promise<void> {
+
+  clearPersistence(): Promise<void> {
     throw new Error("Method not implemented.");
   }
+
+  enablePersistence(settings?: firebase.firestore.PersistenceSettings): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  enableNetwork(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  disableNetwork(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
   collection(collectionPath: string): MockCollectionReference<firebase.firestore.DocumentData> {
     const parts = collectionPath.split("/");
     if (parts.length % 2 === 0) {
@@ -33,9 +51,11 @@ export class MockFirestore implements firebase.firestore.Firestore {
       DEFAULT_DATA_CONVERTER
     );
   }
+
   collectionGroup(collectionId: string): firebase.firestore.Query<firebase.firestore.DocumentData> {
     throw new Error("Method not implemented.");
   }
+
   doc(documentPath: string): MockDocumentReference<firebase.firestore.DocumentData> {
     const parts = documentPath.split("/");
     if (parts.length % 2 !== 0) {
@@ -48,37 +68,30 @@ export class MockFirestore implements firebase.firestore.Firestore {
       DEFAULT_DATA_CONVERTER
     );
   }
+
   runTransaction<T>(
     updateFunction: (transaction: firebase.firestore.Transaction) => Promise<T>
   ): Promise<T> {
     throw new Error("Method not implemented.");
   }
-  batch(): firebase.firestore.WriteBatch {
-    throw new Error("Method not implemented.");
-  }
-  clearPersistence(): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-  enableNetwork(): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-  disableNetwork(): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
-  waitForPendingWrites(): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
+
   onSnapshotsInSync(observer: {
-    next?: ((value: void) => void) | undefined;
-    error?: ((error: Error) => void) | undefined;
-    complete?: (() => void) | undefined;
+    next?: (value: void) => void;
+    error?: (error: Error) => void;
+    complete?: () => void;
   }): () => void;
   onSnapshotsInSync(onSync: () => void): () => void;
   onSnapshotsInSync(onSync: any): () => void {
     throw new Error("Method not implemented.");
   }
+
+  settings(settings: firebase.firestore.Settings): void {}
+
   terminate(): Promise<void> {
-    throw new Error("Method not implemented.");
+    return Promise.resolve();
   }
-  INTERNAL: { delete: () => Promise<void> };
+
+  waitForPendingWrites(): Promise<void> {
+    return Promise.resolve();
+  }
 }
