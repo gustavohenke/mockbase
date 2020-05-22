517419e9822dfb83d8703c55b84de3b1c5368756
Gustavo Henke <guhenke@gmail.com>
2020-05-22 14:24:55 +1000

    refactor(firestore): remove DataContainer interface

--- firestore/data-container.ts
+++
@@ -1,6 +0,0 @@
-import { DocumentReference } from "./document-reference";
-import { CollectionReference } from "./collection-reference";
-
-export interface DataContainer<T extends DocumentReference<any> | CollectionReference<any>> {
-  children: Map<string, T>;
-}
