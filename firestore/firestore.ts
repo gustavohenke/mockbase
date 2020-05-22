71fcc9db4ff3f8a161506167bd2995ee28539817
Gustavo Henke <guhenke@gmail.com>
2020-05-22 14:01:02 +1000

    feat(firestore): add unimplemented Firestore

--- firestore/firestore.ts
+++ firestore/firestore.ts
@@ -1,104 +1,58 @@
 import * as firebase from "firebase";
-import { MockApp } from "../app";
-import { EventEmitter } from "../util";
-import { CollectionReference } from "./collection-reference";
-import { DataContainer } from "./data-container";
-import { DocumentReference } from "./document-reference";
-import { Query } from "./query";
-import { DataConverter } from "./data-converter";
 
-export class MockFirestore
-  implements firebase.firestore.Firestore, DataContainer<CollectionReference> {
-  // ?
-  INTERNAL: { delete: () => Promise<void> };
+export class MockFirestore implements firebase.firestore.Firestore {
+  public readonly documentData = new Map<string, firebase.firestore.DocumentData>();
 
-  private id = 0;
-  public readonly children = new Map<string, CollectionReference>();
-  public readonly data = new Map<string, firebase.firestore.DocumentData>();
-  public readonly collectionEvents = new Map<string, EventEmitter>();
+  constructor(public readonly app: firebase.app.App) {}
 
-  constructor(public readonly app: MockApp) {}
-
-  batch(): firebase.firestore.WriteBatch {
+  settings(settings: firebase.firestore.Settings): void {
     throw new Error("Method not implemented.");
   }
-
-  clearPersistence(): Promise<void> {
-    return Promise.resolve();
-  }
-
-  enablePersistence(settings?: firebase.firestore.PersistenceSettings): Promise<void> {
-    return Promise.resolve();
-  }
-
-  enableNetwork(): Promise<void> {
-    return Promise.resolve();
-  }
-
-  disableNetwork(): Promise<void> {
-    return Promise.resolve();
-  }
-
-  private child(path: string[]) {
-    const converter = new DataConverter();
-    return path.reduce(
-      (container, part, i) =>
-        i % 2 === 0
-          ? new CollectionReference(this, part, i === 0 ? null : container, converter)
-          : new DocumentReference(this, part, container, converter),
-      this as any
-    );
-  }
-
-  collection(collectionPath: string): CollectionReference {
-    const path = collectionPath.split("/").filter(Boolean);
-    if (path.length % 2 === 0) {
-      throw new Error("Not a collection");
-    }
-
-    return this.child(path);
-  }
-
-  collectionGroup(collectionId: string): Query {
+  enablePersistence(settings?: firebase.firestore.PersistenceSettings | undefined): Promise<void> {
     throw new Error("Method not implemented.");
   }
-
-  doc(documentPath: string): DocumentReference {
-    const path = documentPath.split("/").filter(Boolean);
-    if (path.length % 2 === 1) {
-      throw new Error("Not a document");
-    }
-
-    return this.child(path);
-  }
-
-  nextId() {
-    return "__id" + this.id++;
-  }
-
-  onSnapshotsInSync(observer: {
-    next?: (value: void) => void;
-    error?: (error: Error) => void;
-    complete?: () => void;
-  }): () => void;
-  onSnapshotsInSync(onSync: () => void): () => void;
-  onSnapshotsInSync(observerOrOnSync: any): () => void {
+  collection(
+    collectionPath: string
+  ): firebase.firestore.CollectionReference<firebase.firestore.DocumentData> {
     throw new Error("Method not implemented.");
   }
-
+  doc(documentPath: string): firebase.firestore.DocumentReference<firebase.firestore.DocumentData> {
+    throw new Error("Method not implemented.");
+  }
+  collectionGroup(collectionId: string): firebase.firestore.Query<firebase.firestore.DocumentData> {
+    throw new Error("Method not implemented.");
+  }
   runTransaction<T>(
     updateFunction: (transaction: firebase.firestore.Transaction) => Promise<T>
   ): Promise<T> {
     throw new Error("Method not implemented.");
   }
-
-  settings(settings: firebase.firestore.Settings): void {}
-
-  terminate(): Promise<void> {
-    return Promise.resolve();
+  batch(): firebase.firestore.WriteBatch {
+    throw new Error("Method not implemented.");
   }
-
+  clearPersistence(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  enableNetwork(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  disableNetwork(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
   waitForPendingWrites(): Promise<void> {
-    return Promise.resolve();
+    throw new Error("Method not implemented.");
   }
+  onSnapshotsInSync(observer: {
+    next?: ((value: void) => void) | undefined;
+    error?: ((error: Error) => void) | undefined;
+    complete?: (() => void) | undefined;
+  }): () => void;
+  onSnapshotsInSync(onSync: () => void): () => void;
+  onSnapshotsInSync(onSync: any): () => void {
+    throw new Error("Method not implemented.");
+  }
+  terminate(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  INTERNAL: { delete: () => Promise<void> };
 }
