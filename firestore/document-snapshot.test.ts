2bc1bb5a6feee414da63718c994da315103d1e46
Gustavo Henke <guhenke@gmail.com>
2020-05-24 13:59:59 +1000

    test(firestore): update tests for DocumentSnapshot

--- firestore/document-snapshot.test.ts
+++ firestore/document-snapshot.test.ts
@@ -1,48 +1,54 @@
 import { MockApp } from "../app/app";
-import { DocumentReference, DocumentSnapshot } from "./";
-import { DataConverter } from "./data-converter";
+import { MockDocumentReference } from "./";
+import { MockDocumentSnapshot } from "./document-snapshot";
 
-let ref: DocumentReference;
-beforeEach(() => {
+let ref: MockDocumentReference;
+beforeEach(async () => {
   const app = new MockApp("app");
   ref = app.firestore().doc("foo/bar");
 });
 
-it("exposes #firestore", () => {
-  const doc = new DocumentSnapshot(ref, {}, new DataConverter());
-  expect(doc.firestore).toBe(ref.firestore);
-});
-
 it("exposes #ref", () => {
-  const doc = new DocumentSnapshot(ref, {}, new DataConverter());
+  const doc = new MockDocumentSnapshot(ref, {});
   expect(doc).toHaveProperty("ref", ref);
 });
 
 it("exposes #id", () => {
-  const doc = new DocumentSnapshot(ref, {}, new DataConverter());
+  const doc = new MockDocumentSnapshot(ref, {});
   expect(doc.id).toBe("bar");
 });
 
 describe("#data()", () => {
   it("returns the whole data", () => {
-    const doc = new DocumentSnapshot(ref, { foo: "bar" }, new DataConverter());
+    const doc = new MockDocumentSnapshot(ref, { foo: "bar" });
     expect(doc.data()).toEqual({ foo: "bar" });
+  });
+
+  it("returns converted data from the DocumentReference", () => {
+    const converter = {
+      fromFirestore: jest.fn().mockReturnValue({ baz: "qux" }),
+      toFirestore: jest.fn(),
+    };
+    const refWithConverter = ref.withConverter(converter);
+    const doc = new MockDocumentSnapshot(refWithConverter, { foo: "bar" });
+    expect(doc.data()).toEqual({ baz: "qux" });
+    expect(converter.fromFirestore).toHaveBeenCalled();
   });
 });
 
 describe("#get()", () => {
   it("gets a specific key", () => {
-    const doc = new DocumentSnapshot(ref, { foo: "bar" }, new DataConverter());
+    const doc = new MockDocumentSnapshot(ref, { foo: "bar" });
     expect(doc.get("foo")).toBe("bar");
   });
 
   it("gets a deep-level key", () => {
-    const doc = new DocumentSnapshot(ref, { foo: { bar: "baz" } }, new DataConverter());
+    const doc = new MockDocumentSnapshot(ref, { foo: { bar: "baz" } });
     expect(doc.get("foo.bar")).toBe("baz");
   });
 
   it("gets a deep-level key that doesn't exist as undefined", () => {
-    const doc = new DocumentSnapshot(ref, { foo: "bar" }, new DataConverter());
+    const doc = new MockDocumentSnapshot(ref, { foo: "bar" });
     expect(doc.get("foo.bar")).toBe(undefined);
   });
 });
